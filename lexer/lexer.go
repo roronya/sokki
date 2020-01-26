c9a3d9e250454aaf5c37adc4e4cdb6df4705b75c
roronya <roronya628@gmail.com>
2020-01-27 01:28:47 +0900

    add lexer

---
+++ lexer/lexer.go
@@ -0,0 +1,43 @@
+package lexer
+
+import (
+	"regexp"
+
+	"github.com/roronya/sokki/token"
+)
+
+var SHIFT_REGEXP = regexp.MustCompile(`^ >$`)
+var MORESHIFT_REGEXP = regexp.MustCompile(`^ >>$`)
+var PARAGRAPH_REGEXP = regexp.MustCompile(`^([^\n]*)`)
+
+type Lexer struct {
+	input    []rune
+	position int
+}
+
+func New(input string) *Lexer {
+	l := &Lexer{input: []rune(input), position: 0}
+	return l
+}
+
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+	if l.input[l.position] == '\n' {
+		l.position++
+		return newToken(token.NEWLINE, l.input[l.position-1:l.position])
+	}
+	// 正規表現で抜き出す
+	s := string(l.input[l.position:])
+	pr := PARAGRAPH_REGEXP.FindStringSubmatch(s)
+
+	if pr != nil {
+		size := len([]rune(pr[1]))
+		l.position += size
+		return newToken(token.PARAGRAPH, l.input[l.position-size:l.position])
+	}
+	return tok
+}
+
+func newToken(tokenType token.TokenType, literal []rune) token.Token {
+	return token.Token{Type: tokenType, Literal: string(literal)}
+}
---
+++ lexer/lexer_test.go
@@ -0,0 +1,49 @@
+package lexer
+
+import (
+	"testing"
+
+	"github.com/roronya/sokki/token"
+)
+
+func TestNew(t *testing.T) {
+	input := "abc"
+	l := New(input)
+
+	if string(l.input) != "abc" {
+		t.Fatalf("input is not abc. got=%s", string(l.input))
+	}
+
+	if l.position != 0 {
+		t.Fatalf("position is not 0. got=%d", l.position)
+	}
+
+}
+
+func TestNextToken(t *testing.T) {
+	input := `マリア様の庭に集う少女たちが、
+今日も天使のような無垢な笑顔で、
+背の高い門をくぐり抜けていく。
+`
+
+	tests := []struct {
+		expectedType    token.TokenType
+		expectedLiteral string
+	}{
+		{token.PARAGRAPH, "マリア様の庭に集う少女たちが、"},
+		{token.NEWLINE, "\n"},
+		{token.PARAGRAPH, "今日も天使のような無垢な笑顔で、"},
+		{token.NEWLINE, "\n"},
+		{token.PARAGRAPH, "背の高い門をくぐり抜けていく。"},
+	}
+
+	l := New(input)
+
+	for i, tt := range tests {
+		tok := l.NextToken()
+
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d] - Literal wrong. expected=%q, got=%q", i, tt.expectedLiteral, tok.Literal)
+		}
+	}
+}
