7f1f757cde30fdc84b9fffa406a00843da7a73db
roronya <roronya628@gmail.com>
2020-02-01 03:11:24 +0900

    modify Eval

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -11,7 +11,7 @@ func Eval(node ast.Node) string {
 	case *ast.Document:
 		return evalDocument(node)
 	case *ast.Paragraph:
-		return fmt.Sprintf("<p>%s</p>", node.Value)
+		return fmt.Sprintf("<p>%s</p>\n", node.Value)
 	}
 	return ""
 }
@@ -27,15 +27,30 @@ grid-template-columns: 1fr 1fr 1fr;
 </style>
 <body>
 `
-	for _, s := range node.Left {
-		html += Eval(&s)
+	// HTMLの上から順番に選択されるので、各カラムを混ぜずに固めておく
+	for _, s := range node.Sections {
+		html += fmt.Sprintf("<div style=\"grid-column: %d; grid-row: %d;\">\n", 0, s.Id)
+		for _, pr := range s.Left {
+			html += Eval(pr)
+		}
+		html += "</div>\n"
 	}
-	for _, s := range node.Middle {
-		html += Eval(&s)
+
+	for _, s := range node.Sections {
+		html += fmt.Sprintf("<div style=\"grid-column: %d; grid-row: %d;\">\n", 1, s.Id)
+		for _, pr := range s.Middle {
+			html += Eval(pr)
+		}
+		html += "</div>\n"
 	}
-	for _, s := range node.Right {
-		html += Eval(&s)
+
+	for _, s := range node.Sections {
+		html += fmt.Sprintf("<div style=\"grid-column: %d; grid-row: %d;\">\n", 2, s.Id)
+		for _, pr := range s.Right {
+			html += Eval(pr)
+		}
+		html += "</div>\n"
 	}
-	html += "</body></html>"
+	html += "</body>\n</html>"
 	return html
 }
--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -8,26 +8,34 @@ import (
 )
 
 func TestEval(t *testing.T) {
-	input := "私立リリアン女学園。ここは乙女の園。"
+	input := `マリア様の庭に集う乙女達が、
+今日も天使のような無垢な笑顔で、
+背の高い門をくぐり抜けていく。`
 	l := lexer.New(input)
 	p := parser.New(l)
 	ast := p.ParseDocument()
 
 	result := Eval(ast)
 	if result != `<html>
+<head>
+<style>
+body {
+display: grid;
+grid-template-columns: 1fr 1fr 1fr;
+}
+</style>
 <body>
-<section>
-<p>私立リリアン女学園。ここは乙女の園。</p>
-</section>
-<section>
-
-</section>
-<section>
-
-</section>
+<div style="grid-column: 0; grid-row: 0;">
+<p>マリア様の庭に集う乙女達が、</p>
+<p>今日も天使のような無垢な笑顔で、</p>
+<p>背の高い門をくぐり抜けていく。</p>
+</div>
+<div style="grid-column: 1; grid-row: 0;">
+</div>
+<div style="grid-column: 2; grid-row: 0;">
+</div>
 </body>
-</html>
-` {
+</html>` {
 		t.Errorf("result is invalid. got=%s", result)
 	}
 }
